3fa69a9f527a2e5711c95b258359e5d517672f06
EvgheniAdvahov <evghenii.88@gmail.com>
2024-03-12 11:32:30 +0200

    hw3 first commit

---
+++ hw3/src/main/java/org/example/Calculator.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Calculator {
+
+    public static <T extends Number> Number sum(T firstNum, T secondNum) {
+        if (firstNum instanceof Double || secondNum instanceof Double) {
+            return firstNum.doubleValue() + secondNum.doubleValue();
+        } else if (firstNum instanceof Float || secondNum instanceof Float) {
+            return firstNum.floatValue() + secondNum.floatValue();
+        } else if (firstNum instanceof Long || secondNum instanceof Long) {
+            return firstNum.longValue() + secondNum.longValue();
+        } else {
+            return firstNum.intValue() + secondNum.intValue();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Calculator.sum(5,6) = " + Calculator.sum(5f, 6.1));
+    }
+
+}
